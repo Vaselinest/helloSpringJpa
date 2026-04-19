90d13accb5536fb736e2005a00b95b9bb8d6c3ed
최주성 <wntjd873@gmail.com>
2026-04-19 21:30:38 +0900

    feat: 푸터 시간 표시 및 Category 비즈니스 로직 구현

--- src/main/java/kr/ac/hansung/cse/service/ProductService.java
+++ src/main/java/kr/ac/hansung/cse/service/ProductService.java
@@ -1,5 +1,6 @@
 package kr.ac.hansung.cse.service;
 
+import kr.ac.hansung.cse.exception.DuplicateCategoryException;
 import kr.ac.hansung.cse.model.Category;
 import kr.ac.hansung.cse.model.Product;
 import kr.ac.hansung.cse.repository.CategoryRepository;
@@ -132,5 +133,21 @@ public class ProductService {
     @Transactional
     public void deleteProduct(Long id) {
         productRepository.delete(id);
+    }
+
+    // 기존 getAllCategories() 아래에 추가
+    @Transactional
+    public Category createCategory(String name) {
+        categoryRepository.findByName(name)
+                .ifPresent(c -> { throw new DuplicateCategoryException(name); });
+        return categoryRepository.save(new Category(name));
+    }
+
+    @Transactional
+    public void deleteCategory(Long id) {
+        long count = categoryRepository.countProductsByCategoryId(id);
+        if (count > 0) throw new IllegalStateException(
+                "상품 " + count + "개가 연결되어 있어 삭제할 수 없습니다.");
+        categoryRepository.delete(id);
     }
 }
